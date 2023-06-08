abc9a9201b286740f306e1f49fa11352b9a8bd1c
camila de souza <camila.sparquitetura@gmail.com>
2023-06-07 22:35:12 -0300

    feat: buttons styles and structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
 import { PageRoutes } from "./routes";
 import { ToastConfig } from "./components/ToastConfig";
+import { Button } from "./components/Buttons";
 
 export const App = () => {
+  function userSell(): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <BrowserRouter>
       <ToastConfig />
+      <Button
+        onClick={() => userSell()}
+        size="default"
+        color="gray6"
+        content={"Cadastre-se"}
+      />
       <PageRoutes />
     </BrowserRouter>
   );
--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -2,8 +2,27 @@ import { StyledButton } from "./style";
 
 interface iButtonProps {
   content: string | React.ReactNode;
-  size: "default" | "medium";
-  color: "primary" | "gray" | "disable";
+  size: "default" | "large";
+  color:
+    | "gray0"
+    | "gray1"
+    | "gray5"
+    | "gray_white_5"
+    | "gray6"
+    | "gray8"
+    | "gray10"
+    | "brand1"
+    | "brand2"
+    | "brand3"
+    | "brand4"
+    | "alert1"
+    | "alert2"
+    | "success1"
+    | "success2"
+    | "border_dark_gray"
+    | "border_ligth_gray"
+    | "border_blue"
+    | "disable";
   onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
