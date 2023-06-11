96caccf23555b204e3df966b030c48fec3a1daf9
Maikol Santos <maikol.lourencon@gmail.com>
2023-06-11 18:46:06 -0300

    feat: cria componente container para formulários

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,11 @@ import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
 import { PageRoutes } from "./routes";
 import { ToastConfig } from "./components/ToastConfig";
-import { Input } from "./components/Input";
-import { useForm } from "react-hook-form";
 
 export const App = () => {
-  const { register } = useForm();
-
   return (
     <BrowserRouter>
       <ToastConfig />
-      <Input
-        label="Teste label"
-        placeholder="Placeholder"
-        register={register("teste")}
-        error="Mensagem de erro"
-      />
       <PageRoutes />
     </BrowserRouter>
   );
