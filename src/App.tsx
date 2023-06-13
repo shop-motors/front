90ba57cd4ed54589c9bc8f72f9becd65019af17f
RicardoAymay <r.aymay.goncalves@gmail.com>
2023-06-13 14:42:46 -0300

    commit apenas para fazer pull

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
 import { PageRoutes } from "./routes";
 import { ToastConfig } from "./components/ToastConfig";
+import Modal from "./components/Modals";
+
 
 export const App = () => {
   return (
