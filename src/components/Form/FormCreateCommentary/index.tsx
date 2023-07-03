29a43b4aba03ec5caf65c2d40184e65bacaac8ce
Maikol Santos <maikol.lourencon@gmail.com>
2023-07-03 20:42:06 -0300

    feat: add default sentences in commentary form

--- src/components/Form/FormCreateCommentary/index.tsx
+++ src/components/Form/FormCreateCommentary/index.tsx
@@ -1,4 +1,4 @@
-import { Container, UserContainer } from "./styles";
+import { Container, SentencesContainer, UserContainer } from "./styles";
 import ImgProfile from "../../../images/leia.jpeg";
 import { Form } from "..";
 import { Input } from "../../Input";
@@ -6,17 +6,27 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { createCommentarySchema } from "./createCommentarySchema";
 import { Button } from "../../Buttons";
+import { useState } from "react";
 
 interface IComment {
   content: string;
 }
 
 export const FormCreateCommentary = () => {
+  const [currentSentence, setCurrentSentence] = useState("");
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ resolver: yupResolver(createCommentarySchema) });
+    setValue,
+  } = useForm<IComment>({ resolver: yupResolver(createCommentarySchema) });
+
+  const sentences = [
+    "Gostei muito!",
+    "Incrível",
+    "Recomendarei para meus amigos!",
+  ];
 
   const dataUserMock = {
     img: ImgProfile,
@@ -25,6 +35,12 @@ export const FormCreateCommentary = () => {
 
   const handleCreateCommentary: SubmitHandler<IComment> = (data: IComment) => {
     console.log(data);
+  };
+
+  const setSentences = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setValue("content", event.currentTarget.innerText);
   };
 
   return (
@@ -45,6 +61,16 @@ export const FormCreateCommentary = () => {
         />
         <Button color="brand1" content="Comentar" size="large" type="submit" />
       </Form>
+
+      <SentencesContainer>
+        {sentences.map((sentence, index) => {
+          return (
+            <button onClick={setSentences} key={index}>
+              {sentence}
+            </button>
+          );
+        })}
+      </SentencesContainer>
     </Container>
   );
 };
